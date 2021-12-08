094c0ea0cdf30a1ab94b86d0b5c5f7ea098aa37d
andiledub <andile_dube@outlook.com>
2021-12-08 19:52:36 +0200

    ValR Market Data Download#1

--- components/library/MarketData.py
+++ components/library/MarketData.py
@@ -1,106 +1,112 @@
 import asyncio
-from datetime import datetime
 import json
 import logging
-from os import name
-import time
+from datetime import datetime
+from os import name 
 from collections import defaultdict
 from decimal import Decimal
-from typing import Sequence
+import time
 from websocket import create_connection
 import sys
 
-async def connect():
-    url = url = "wss://ws.luno.com/api/1/stream/XBTZAR"
-    websoc = await websockets.connect(url)
-    await websoc.send(json.dumps({
-        'api_key_id': 'heauh4f78vajn',
-        'api_key_secret': 'qkvSG6T01SDWPP7UsmRfMAuZI5gI8kkow_dC7tdZOtc'
-    }))
-
-    initial = await websoc.recv()
-    initial_data = json.loads(initial)
-    asks = {x['id']: [Decimal(x['price']), Decimal(x['volume'])] for x in initial_data['asks']}
-    bids = {x['id']: [Decimal(x['price']), Decimal(x['volume'])] for x in initial_data['bids']}
-    return asks
+logging.basicConfig(level=logging.DEBUG, format = '%(asctime)s - %(levelname)s - %(message)s')
 
 
-data = asyncio.run(connect())
-print(data)
+class MarketData:
+    def __init__(self, source,ticker, api_key=None, api_secret=None) -> None:
+        self.ticker = ticker.upper()
+        self.source = source.upper()
+        self.api_key = api_key
+        self.api_secret = api_secret
+        self.sequence = None
+        self.bid = {}
+        self.ask = {}
+        self.api_book = {"bids": {}, "asks": {}, "timestamp": {}}
+        self.api_feed = "MARKET_SUMMARY_UPDATE"
+        self.websocket = None
 
+    def urlSwitcher(self):
+        switcher = {
+            'LUNO': "wss://ws.luno.com/api/1/stream/" + self.ticker,
+            'VALR': "wss://api.valr.com/ws/trade"
+        }
+        if self.source in switcher:
+            return switcher.get(self.source)
+        else:
+            logging.error("Invalid Market Data Source")
+            sys.exit(1)
+    
+    def messageHandler(self):
+        api_feed = "AGGREGATED_ORDERBOOK_UPDATE"
+        switcher = {
+            'LUNO': json.dumps({
+                                'api_key_id': 'heauh4f78vajn',
+                                'api_key_secret': 'qkvSG6T01SDWPP7UsmRfMAuZI5gI8kkow_dC7tdZOtc'}),
+            'VALR': '{"type":"SUBSCRIBE", "subscriptions": [{"event": "%(feed1)s","pairs": ["%(symbol)s"]}]}'\
+                        % {"feed1": api_feed, "symbol": self.ticker}
+        }
 
+        return switcher.get(self.source, "Invalid Market Data Source")
+    
+    def api_update_book(self,data, side, update_time):
+        for x in data:
+            price_level = x['price']
+            size = float(x['quantity'])
+            if size != 0.0:
+                self.api_book[side].update({price_level : {'volume': size}})
 
-domain = "wss://api.valr.com/ws/trade"
-api_feed = "MARKET_SUMMARY_UPDATE"
-api_symbol = "BTCZAR"
-api_data = '{"type":"SUBSCRIBE", "subscriptions": [{"event": "%(feed1)s","pairs": ["%(symbol)s"]}]}'\
-        % {"feed1": api_feed, "symbol": api_symbol}
-api_book = {"bids": {}, "asks": {}, "timestamp": {}}
-
-def api_update_book(data, side, update_time):
-    for x in data:
-        price_level = x['price']
-        size = float(x['quantity'])
-        if size != 0.0:
-            api_book[side].update({price_level : {'volume': size}})
-
-
-if __name__=='__main__':
-    try:
-        ws = create_connection(domain)
-        print("Connection Established ...")
-    except Exception as error:
-        print("WebSocket Connection failed (%s)" % error)
-        ws.close()
-        sys.exit(1)
-
-    start_time = datetime.now()
-
-
-    try:
-        ws.send(api_data)
-    except Exception as error:
-        print("Feed subscription failed (%s)" % error)
-        ws.close()
-        sys.exit(1)
-
-
-    ping = '{"type": "PING"}'
-    while True:
+    def connect(self):
         try:
-            api_data = ws.recv()
-        except KeyboardInterrupt:
-            ws.close()
-            sys.exit()
+            self.websocket = create_connection(self.urlSwitcher())
+            logging.info("Connection Established ...")
         except Exception as error:
-            ws.close()
-        print("RECEIVED")
-        print(api_data)
+            logging.error("WebSocket Connection failed (%s)" % error)
+            self.websocket.close()
+            sys.exit(1)
+        
+        start_time = datetime.now()
 
         try:
-            api_data = json.load(api_data)
-        except Exception as ex:
-            print(ex)
-            continue
+            self.websocket.send(self.messageHandler())
+            logging.info("Feed Subcription (%s)" % self.messageHandler())
+        except Exception as error:
+            logging.error("Feed subscription failed (%s)" % error)
+            self.websocket.close()
+            sys.exit(1)
 
-        #Order Count
-        if isinstance(api_data.get("data"), dict):
-            end_time = datetime.now()
-            if (end_time - start_time).total_seconds() > 28:
-                print("Sending PING")
-                ws.send(ping)
-                start_time = end_time
+
+        ping = '{"type": "PING"}'
+        while True:
+            try:
+                api_data = self.websocket.recv()
+            except KeyboardInterrupt:
+                self.websocket.close()
+                sys.exit()
+            except Exception as error:
+                self.websocket.close()
+            logging.info(api_data)
+            
+
+            try:
+                api_data = json.load(api_data)
+            except Exception as ex:
+                #logging.error(ex)
+                continue
+
+            #Order Count
+            if isinstance(api_data.get("data"), dict):
+                end_time = datetime.now()
+                if (end_time - start_time).total_seconds() > 28:
+                    logging.info("Sending PING")
+                    #self.websocket.send(ping)
+                    start_time = end_time
+            
+            if api_data.get("type") == self.api_feed:
+                data = api_data.get("data")
+                self.api_update_book(data["Bids"], "bids", data["LastChange"][:-1])
+                self.api_update_book(data["Asks"], "asks", data["LastChange"][:-1])
+
+test = MarketData(source='Valr', ticker='BTCZAR')
+test.connect()
 
         
-        if api_data.get("type") == api_feed:
-            data = api_data.get("data")
-            api_update_book(data["Bids"], "bids", data["LastChange"][:-1])
-            api_update_book(data["Asks"], "asks", data["LastChange"][:-1])
-
-
-
-
-
-
-
-
---
+++ components/library/__init__.py
@@ -0,0 +1,4 @@
+from .MarketData import MarketData
+from .OrderBook import *
+from . OrderManager import *
+from .TraderApp import * 
